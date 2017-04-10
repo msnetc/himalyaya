4fdc7620fb58cad7cdfef718bf43c9ed989ac0bc
jerry <83814616@qq.com>
2017-04-10 13:09:04 +0800

    add

--- src/main/java/com/wangnian/mybatis/SpringbootmybatisApplication.java
+++ src/main/java/com/wangnian/mybatis/SpringbootmybatisApplication.java
@@ -1,26 +1,13 @@
 package com.wangnian.mybatis;
 
-import com.wangnian.mybatis.entity.User;
-import com.wangnian.mybatis.mapper.UserDaoMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootmybatisApplication implements CommandLineRunner {
+public class SpringbootmybatisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootmybatisApplication.class, args);
     }
 
-    @Autowired
-    private UserDaoMapper userDao1;
-
-    @Override
-    public void run(String... strings) throws Exception {
-        User user1 = userDao1.selectUserById(1);
-        System.out.println(user1.getUserName());
-
-    }
 }
--- src/test/java/com/wangnian/mybatis/SpringbootmybatisApplicationTests.java
+++ src/test/java/com/wangnian/mybatis/SpringbootmybatisApplicationTests.java
@@ -1,14 +1,28 @@
 package com.wangnian.mybatis;
 
+import com.wangnian.mybatis.entity.User;
+import com.wangnian.mybatis.mapper.UserDaoMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringbootmybatisApplication.class)
 public class SpringbootmybatisApplicationTests {
 
+
+	@Autowired
+	private UserDaoMapper userDao1;
+
+	@Test
+	public void getUser(){
+		User user1 = userDao1.selectUserById(9);
+		Assert.notNull(user1);
+		User Use2 = userDao1.selectUserById(8);
+	}
 	@Test
 	public void contextLoads() {
 	}
